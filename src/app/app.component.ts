9b8172fb46249b9708bba06ba1250c09102f5de7
Okigbo <okigbookey@gmail.com>
2018-04-09 13:18:42 -0400

    Feat: Add *ngif directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,30 @@ export class AppComponent {
   title = 'My New Angular App!';
   name = 'Joshua';
 
+  public persons = [
+    {
+      'name': 'Joshua Emanaul',
+      'company': 'NewAge Computer Inc.'
+    },
+    {
+      'name': 'Mark Goodwill',
+      'company': 'FedEx Inc.'
+    },
+    {
+      'name': 'Paul Elizabeth',
+      'company': 'ComputerRE Ent.'
+    },
+    {
+      'name': 'Mary Oliva',
+      'company': 'Advanced Diagnostics'
+    }
+  ];
+
   alertName(): void {
     alert(`${this.name} was clicked`);
+  }
+
+  displayPerson(persons) {
+    return `${persons.name} works for ${persons.company}`;
   }
 }
