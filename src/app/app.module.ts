6dc3998e8d7ede6346fa3cc63cf7093f3a616dc5
Okigbo <okigbookey@gmail.com>
2018-04-09 12:33:21 -0400

    Feat: Import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { Header1Component } from './header1/header1.component';
@@ -12,7 +12,8 @@ import { Header1Component } from './header1/header1.component';
     Header1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
