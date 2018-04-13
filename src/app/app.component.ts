0cfa7b46c5ae1206fd06b7fc146d3bd76c49d698
Okigbo <okigbookey@gmail.com>
2018-04-13 09:21:59 -0400

    Feat: Update ap.component.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'My New Angular App!';
   name = 'Joshua';
-  page = null;
+
 
 
   alertName(): void {
     alert(`${this.name} was clicked`);
   }
 
-  changePage1() {
-    if (this.page === 0) {
-      return this.page = 2;
-    }
-    if (this.page < 6) {
-      return this.page += 1;
-    } else {
-      return this.page = 6;
-    }
-  }
-
-  changePage() {
-    if (this.page === 6) {
-      return this.page = 4;
-    }
-    if (this.page > 1) {
-      return this.page -= 1;
-    } else {
-      return this.page = 0;
-    }
-  }
 }
--- src/app/switch-component/switch-component.component.ts
+++ src/app/switch-component/switch-component.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchComponentComponent implements OnInit {
 
+  page = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changePage1() {
+    if (this.page === 0) {
+      return this.page = 2;
+    }
+    if (this.page < 6) {
+      return this.page += 1;
+    } else {
+      return this.page = 6;
+    }
+  }
+
+  changePage() {
+    if (this.page === 6) {
+      return this.page = 4;
+    }
+    if (this.page > 1) {
+      return this.page -= 1;
+    } else {
+      return this.page = 0;
+    }
   }
 
 }
